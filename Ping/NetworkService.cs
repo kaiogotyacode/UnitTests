9bce51ea1a1063225b176d9d20812313ca9c6f79
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-10-04 08:11:58 -0300

    Created NetworkServices and separated in two different projects. Testing Environment and Concrete Classes

---
+++ Ping/NetworkService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.NetworkInformation;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing() => "Success: Ping Sent!";
+
+        public int PingTimeout(int a, int b) => a + b;
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.UtcNow;
+        }
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                Ttl = 1,
+                DontFragment = true
+            };
+        }
+    }
+}
